e57b986c0311f47b0543080f4d47b3c92af2792d
Maksim Borovik <mouxez@gmail.com>
2020-05-25 20:50:26 +0300

    add main.js

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Title from './Title.js';
+
+export default class Main extends Component {
+    render() {
+      return (
+        <div>
+          <Title title={'ToDos'}/>
+        </div>
+      )
+    }
+  }
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Title extends Component {
+    render() {
+      return (
+      <h1>{this.props.title}</h1>
+      )
+    }
+  }
+  
